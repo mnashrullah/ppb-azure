59f4538d8911c8c3b9676639832b2e07e934daf6
Muhammad Nashrullah <muhammadnashrullah.it@gmail.com>
2019-04-09 12:01:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -43,7 +43,11 @@
                     // Include config file
                     require_once "dbconfig.php";
                     
-                    $sql= "select * from portfolio";
+                    $tsql= "SELECT * from portfolio";
+                    // $tsql= "SELECT TOP 20 pc.Name as CategoryName, p.name as ProductName
+                    //      FROM [SalesLT].[ProductCategory] pc
+                    //      JOIN [SalesLT].[Product] p
+                    //      ON pc.productcategoryid = p.productcategoryid";
                     $getResults= sqlsrv_query($conn, $tsql);
                     echo ("Data" . PHP_EOL);
                     if ($getResults == FALSE)
@@ -52,6 +56,16 @@
                      echo ($row['id'] . " " . $row['name'] . PHP_EOL);
                     }
                     sqlsrv_free_stmt($getResults);
+                    
+                    // $sql= "select * from portfolio";
+                    // $getResults= sqlsrv_query($conn, $tsql);
+                    // echo ("Data" . PHP_EOL);
+                    // if ($getResults == FALSE)
+                    //     echo (sqlsrv_errors());
+                    // while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+                    //  echo ($row['id'] . " " . $row['name'] . PHP_EOL);
+                    // }
+                    // sqlsrv_free_stmt($getResults);
 
 
 
