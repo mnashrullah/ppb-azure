b3a20164020057c42831d8a6e02ebcd943fd1120
Muhammad Nashrullah <muhammadnashrullah.it@gmail.com>
2019-04-09 16:16:15 +0700

    fix

--- index.php
+++ index.php
@@ -52,6 +52,7 @@
                             echo "<th>#</th>";
                             echo "<th>Name</th>";
                             echo "<th>Description</th>";
+                            echo "<th>Date Created</th>";
                             echo "<th>Action</th>";
                         echo "</tr>";
                     echo "</thead>";
@@ -61,6 +62,7 @@
                             echo "<td>" . $row['id'] . "</td>";
                             echo "<td>" . $row['name'] . "</td>";
                             echo "<td>" . $row['description'] . "</td>";
+                            echo "<td>" . $row['dateCreated'] . "</td>";
                             echo "<td>";
                                 // echo "<a href='update.php?id=". $row['id'] ."' title='Update Record' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
                                 echo "<a href='delete.php?id=". $row['id'] ."' title='Delete Record' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
