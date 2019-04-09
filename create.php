3d06ad6810caad3ef78c1b52f26cb7f7f6cafbc6
Muhammad Nashrullah <muhammadnashrullah.it@gmail.com>
2019-04-09 16:32:28 +0700

    Update create.php

--- create.php
+++ create.php
@@ -26,7 +26,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }
     // Check input errors before inserting in database
     if(empty($name_err) && empty($description_err)){
-        $date = date("YYYY-mm-dd");
+        $date = date("Y-m-d");
         $tsql= "INSERT INTO portfolio (name, description, date) VALUES ('$name','$description','$date')";
         
         $getResults= sqlsrv_query($conn, $tsql);
