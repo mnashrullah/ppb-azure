a3df9ba2b8b1b2bf12a2ae19e4a613fadcff23c3
Muhammad Nashrullah <muhammadnashrullah.it@gmail.com>
2019-04-09 16:21:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -62,7 +62,7 @@
                             echo "<td>" . $row['id'] . "</td>";
                             echo "<td>" . $row['name'] . "</td>";
                             echo "<td>" . $row['description'] . "</td>";
-                            echo "<td>" . $row['dateCreated'] . "</td>";
+                            echo "<td>" . $row['description'] . "</td>";
                             echo "<td>";
                                 // echo "<a href='update.php?id=". $row['id'] ."' title='Update Record' data-toggle='tooltip'><span class='glyphicon glyphicon-pencil'></span></a>";
                                 echo "<a href='delete.php?id=". $row['id'] ."' title='Delete Record' data-toggle='tooltip'><span class='glyphicon glyphicon-trash'></span></a>";
