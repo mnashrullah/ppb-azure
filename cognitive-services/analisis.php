d0fa5739e5e7d188ee4d65e5f5654cea1d8e8c97
Muhammad Nashrullah <muhammadnashrullah.it@gmail.com>
2019-04-12 22:57:40 +0700

    add cognitive services

---
+++ cognitive-services/analisis.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Cognitive Services</title>
+     <!-- Required meta tags -->
+     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+    <script src="scripts/analisis.js"></script>
+    
+</head>
+
+<div class="container-fluid">
+  <h1 class="text-primary text-center">Analyze image</h1>
+  
+
+  <div id="imageDiv" style="width:420px">
+        <img id="sourceImage" width="400" />
+    </div>
+    <div id="jsonOutput">
+        Result:
+        <br>
+        <textarea id="responseTextArea" style="width:580px; height:200px;"></textarea>
+    </div>
+</div>
+</html>
---
+++ cognitive-services/scripts/analisis.js
@@ -0,0 +1,70 @@
+window.onload = function () {
+    processImage();
+    function processImage() {
+        // **********************************************
+        // *** Update or verify the following values. ***
+        // **********************************************
+ 
+        // Replace <Subscription Key> with your valid subscription key.
+        var subscriptionKey = "e0d59271b0bd47e2896fc09de4535645";
+ 
+        // You must use the same Azure region in your REST API method as you used to
+        // get your subscription keys. For example, if you got your subscription keys
+        // from the West US region, replace "westcentralus" in the URL
+        // below with "westus".
+        //
+        // Free trial subscription keys are generated in the "westus" region.
+        // If you use a free trial subscription key, you shouldn't need to change
+        // this region.
+        var uriBase =
+            "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
+ 
+        // Request parameters.
+        var params = {
+            "visualFeatures": "Categories,Description,Color",
+            "details": "",
+            "language": "en",
+        };
+ 
+        // Display the image.
+        var sourceImageUrl = "https://ppbazurestorage.blob.core.windows.net/ppbcontainer/20190226093342158d4d6cc4b769f5ead74da9f75a6e1a.png";
+        var divImage = document.getElementById("inputImage");
+        document.querySelector("#sourceImage").src = sourceImageUrl;
+
+        // var expandImg = document.getElementById("sourceImage");
+        // expandImg.src = sourceImageUrl.src;
+ 
+        // Make the REST API call.
+        $.ajax({
+            url: uriBase + "?" + $.param(params),
+ 
+            // Request headers.
+            beforeSend: function(xhrObj){
+                xhrObj.setRequestHeader("Content-Type","application/json");
+                xhrObj.setRequestHeader(
+                    "Ocp-Apim-Subscription-Key", subscriptionKey);
+            },
+ 
+            type: "POST",
+ 
+            // Request body.
+            data: '{"url": ' + '"' + sourceImageUrl + '"}',
+        })
+ 
+        .done(function(data) {
+            // Show formatted JSON on webpage.
+            // $("#responseTextArea").val(JSON.stringify(data, null, 2));
+            var captions = data.description.captions[0].text;
+            $("#responseTextArea").val(JSON.stringify(captions));
+        })
+ 
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            // Display error message.
+            var errorString = (errorThrown === "") ? "Error. " :
+                errorThrown + " (" + jqXHR.status + "): ";
+            errorString += (jqXHR.responseText === "") ? "" :
+                jQuery.parseJSON(jqXHR.responseText).message;
+            alert(errorString);
+        });
+    };
+}
