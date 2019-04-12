1c17b03618330c99b62d4d69d20dfb1a34dd8308
Muhammad Nashrullah <muhammadnashrullah.it@gmail.com>
2019-04-12 23:48:49 +0700

    offlne jquery

--- cognitive-services/analisis.php
+++ cognitive-services/analisis.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <script src="scripts/analisis.js"></script>
     <!-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script> -->
-    <script src="https://code.jquery.com/jquery-3.4.0.min.js"></script>
+    <script src="scripts/jquery-3.4.0.min.js"></script>
     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
